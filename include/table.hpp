c7383811c98ac9f3d712a886fb19410bac41bc56
pauline.requena <pauline.requena@f4401540-0d51-11de-9c5c-7db7966a26d7>
2009-04-01 18:18:15 +0000

    ajout table.hpp

--- include/table.hpp
+++ include/table.hpp
@@ -1,3 +1,100 @@
+
 #ifndef __TABLE__
-  #define __TABLE__
-#endif
+#define	__TABLE__
+
+/*!
+ * \file table.hpp
+ * \brief Implementation du module table qui est un dérivé d'un EcoAgent
+ * \author GDD
+ * \author Arnaud Faure
+ * \author Pauline Requena
+ * \version 0.1
+ * \date 1er avril 2009
+ *
+ * Implementation de la classe Table qui est une classe dérivée de la classe EcoAgent.
+ *
+ */
+
+using namespace std;
+
+ /*! \class Table
+ * \brief Classe dérivée de la classe EcoAgent désignant le Support sur lequel vont être posés les cubes
+ *
+ */
+
+class Table 
+{
+private:
+    
+public:
+    /*!
+     * \brief Constructeur
+     *
+     * Constructeur de la classe Table
+     *
+     *\param id : identifiant unique que l'agent se verra attribuer
+     *
+     */
+    Table(EcoAgentID id);
+
+    /*!
+     * \brief Destructeur
+     *
+     * Destructeur de la classe Table
+     *
+     */
+     ~Table();
+        
+public:
+        /*!
+	 * \brief Suite d'operations realisees par la table lorsqu'elle cherche a fuir
+	 *
+	 *
+	 */
+	virtual void rechercherFuite();
+	
+	/*!
+	 * \brief Suite d'operations realisees par la table lorsqu'elle cherche a se satisfaire
+	 *
+	 *
+	 */
+	virtual void rechercherSatisfaction();
+	
+	/*!
+	 * \brief Suite d'operations realisees lorsque la table agresse un autre eco-agent
+	 *
+	 *
+	 *\param a : EcoAgent a agresser
+         *
+	 */
+	
+	virtual void agresser(EcoAgent a);
+	
+	/*!
+	 * \brief Suite d'operations realisees par la table lorsqu'elle est agressée
+	 *
+	 *
+	 */
+	virtual void estAgresse();
+	
+	/*!
+	 * \brief Suite d'operations realisees par la fuite lorsqu'elle fuit
+	 *
+	 *
+	 */
+	virtual void faireFuite();
+
+	/*!
+	 * \brief Suite d'operations realisees par la table lorsqu'elle se satisfait
+	 *
+	 *
+	 */
+	void faireSatisfaction();
+
+    
+};
+ 
+
+
+#endif	
+
