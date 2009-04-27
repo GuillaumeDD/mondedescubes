508a547ad56dfe665edd232c0178ba5629359133
guillaume.dd <guillaume.dd@f4401540-0d51-11de-9c5c-7db7966a26d7>
2009-04-27 20:23:20 +0000

    Realisation des tests unitaires pour singleton et EcoAgentID

--- trunk/include/ecoAgentIDTest.hpp
+++ trunk/include/ecoAgentIDTest.hpp
@@ -1,28 +1,28 @@
 #ifndef __ECOAGENTIDTEST__
 #define __ECOAGENTIDTEST__
-	#include<cppunit/TestFixture.h>
-	#include<cppunit/extensions/HelperMacros.h>
-	#include "plateformeMondeDesCubes.hpp"
-	#include "ecoAgentID.hpp"
+#include<cppunit/TestFixture.h>
+#include<cppunit/extensions/HelperMacros.h>
+#include "plateformeMondeDesCubes.hpp"
+#include "ecoAgentID.hpp"
 	
-	using namespace std;
+using namespace std;
 
-	class ecoAgentIDTest : public CPPUNIT_NS :: TestFixture
-	{
-    CPPUNIT_TEST_SUITE (ecoAgentIDTest);
-    CPPUNIT_TEST (doTest);
-    CPPUNIT_TEST_SUITE_END ();
+class ecoAgentIDTest : public CPPUNIT_NS :: TestFixture
+{
+  CPPUNIT_TEST_SUITE (ecoAgentIDTest);
+  CPPUNIT_TEST (generateTest);
+  CPPUNIT_TEST (egaliteTest);
+  CPPUNIT_TEST_SUITE_END ();
 
-    public:
-        void setUp (void);
-        void tearDown (void);
+public:
+  void setUp (void);
+  void tearDown (void);
 
-    protected:
-        void doTest (void);
-
-    private:
-		PlateformeMondeDesCubes *p;
-		// Mettre ici les variables dont vous avez besoin pendant les tests
-	};
+protected:
+  void generateTest (void);
+  void egaliteTest (void);
+private:
+  EcoAgentID *eaid1, *eaid2;	  
+};
 
 #endif
