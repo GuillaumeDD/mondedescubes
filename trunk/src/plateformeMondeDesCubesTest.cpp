914cf81d97efc5f6ab1bc9b3f9536b1beca72eb9
guillaume.dd <guillaume.dd@f4401540-0d51-11de-9c5c-7db7966a26d7>
2009-04-29 20:53:48 +0000

    Ajout des tests unitaires obtenirGeneur, obtenirCubePrioritaire, etc. sur l'exemple du rapport avec 3 cubes

--- trunk/src/plateformeMondeDesCubesTest.cpp
+++ trunk/src/plateformeMondeDesCubesTest.cpp
@@ -121,9 +121,9 @@ void plateformeMondeDesCubesTest::getTest(void)
   ea6->setPositionCourante(*(ea5->getId()));
   ea6->setPositionFinale(*(p->getTableID()));
 
-  CPPUNIT_ASSERT(ea1->getEtat() == UNDEFINED && ea2->getEtat() == UNDEFINED && ea3->getEtat() == UNDEFINED && ea4->getEtat() == UNDEFINED);
+  CPPUNIT_ASSERT(ea1->getEtat() == UNDEFINED && ea2->getEtat() == UNDEFINED && ea3->getEtat() == UNDEFINED && ea4->getEtat() == UNDEFINED && ea5->getEtat() == UNDEFINED && ea6->getEtat() == UNDEFINED);
   p->initialiser();
-  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == RECHERCHESATISFACTION && ea3->getEtat() == RECHERCHESATISFACTION && ea4->getEtat() == SATISFAIT);
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == RECHERCHESATISFACTION && ea3->getEtat() == RECHERCHESATISFACTION && ea4->getEtat() == SATISFAIT&& ea5->getEtat() == SATISFAIT && ea6->getEtat() == RECHERCHESATISFACTION);
 
   //test distanceFinaleATable
   CPPUNIT_ASSERT(p->distanceFinaleATable(*(ea1->getId())) == 0);
@@ -157,6 +157,7 @@ void plateformeMondeDesCubesTest::getTest(void)
   ea6->setPositionCourante(*(ea5->getId()));
   ea6->setPositionFinale(*(p->getTableID()));
   p->initialiser();
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == RECHERCHESATISFACTION && ea3->getEtat() == RECHERCHESATISFACTION && ea4->getEtat() == RECHERCHESATISFACTION && ea5->getEtat() == RECHERCHESATISFACTION && ea6->getEtat() == RECHERCHESATISFACTION);
   CPPUNIT_ASSERT(p->obtenirGeneur(*ea1) == NULL);
   CPPUNIT_ASSERT(p->obtenirGeneur(*ea2) == ea4);
   CPPUNIT_ASSERT(p->obtenirGeneur(*ea3) == ea2);
@@ -166,5 +167,185 @@ void plateformeMondeDesCubesTest::getTest(void)
 
   //test obtenirCubePrioritaire
   CPPUNIT_ASSERT(p->obtenirCubePrioritaire() == ea2);
+
+  //test d'une etape de resoudre
+  EcoAgent* temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == RECHERCHESATISFACTION && ea3->getEtat() == RECHERCHESATISFACTION && ea4->getEtat() == RECHERCHEFUITE && ea5->getEtat() == RECHERCHESATISFACTION && ea6->getEtat() == RECHERCHESATISFACTION);
+  CPPUNIT_ASSERT(p->obtenirCubePrioritaire() == ea4);
+
+  //test d'une etape de resoudre
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == RECHERCHESATISFACTION && ea3->getEtat() == RECHERCHESATISFACTION && ea4->getEtat() == RECHERCHEFUITE && ea5->getEtat() == RECHERCHEFUITE && ea6->getEtat() == RECHERCHESATISFACTION);
+  CPPUNIT_ASSERT(p->obtenirCubePrioritaire() == ea5);
+ 
+//test d'une etape de resoudre
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == RECHERCHESATISFACTION && ea3->getEtat() == RECHERCHESATISFACTION && ea4->getEtat() == RECHERCHEFUITE && ea5->getEtat() == RECHERCHEFUITE && ea6->getEtat() == RECHERCHEFUITE);
+  CPPUNIT_ASSERT(p->obtenirCubePrioritaire() == ea6); 
+
+//test d'une etape de resoudre
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == RECHERCHESATISFACTION && ea3->getEtat() == RECHERCHEFUITE && ea4->getEtat() == RECHERCHEFUITE && ea5->getEtat() == RECHERCHEFUITE && ea6->getEtat() == RECHERCHEFUITE);
+  CPPUNIT_ASSERT(p->obtenirCubePrioritaire() == ea3);
+
+//test d'une etape de resoudre
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == RECHERCHESATISFACTION && ea3->getEtat() == RECHERCHESATISFACTION && ea4->getEtat() == RECHERCHEFUITE && ea5->getEtat() == RECHERCHEFUITE && ea6->getEtat() == RECHERCHEFUITE);
+  CPPUNIT_ASSERT(ea3->getPositionCourante() == p->getTableID());
+  CPPUNIT_ASSERT(p->obtenirCubePrioritaire() == ea6);
+
+//test d'une etape de resoudre
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == RECHERCHESATISFACTION && ea3->getEtat() == RECHERCHESATISFACTION && ea4->getEtat() == RECHERCHEFUITE && ea5->getEtat() == RECHERCHEFUITE && ea6->getEtat() == RECHERCHESATISFACTION);
+  CPPUNIT_ASSERT(ea6->getPositionCourante() == p->getTableID());
+  CPPUNIT_ASSERT(p->obtenirCubePrioritaire() == ea5);
+
+//test d'une etape de resoudre
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == RECHERCHESATISFACTION && ea3->getEtat() == RECHERCHESATISFACTION && ea4->getEtat() == RECHERCHEFUITE && ea5->getEtat() == RECHERCHESATISFACTION && ea6->getEtat() == RECHERCHESATISFACTION);
+  CPPUNIT_ASSERT(ea5->getPositionCourante() == p->getTableID());
+  CPPUNIT_ASSERT(p->obtenirCubePrioritaire() == ea4);
+
+//test d'une etape de resoudre
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == RECHERCHESATISFACTION && ea3->getEtat() == RECHERCHESATISFACTION && ea4->getEtat() == RECHERCHESATISFACTION && ea5->getEtat() == RECHERCHESATISFACTION && ea6->getEtat() == RECHERCHESATISFACTION);
+  CPPUNIT_ASSERT(ea4->getPositionCourante() == p->getTableID());
+  CPPUNIT_ASSERT(p->obtenirCubePrioritaire() == ea2);
+
+//test d'une etape de resoudre
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == SATISFAIT && ea3->getEtat() == RECHERCHESATISFACTION && ea4->getEtat() == RECHERCHESATISFACTION && ea5->getEtat() == RECHERCHESATISFACTION && ea6->getEtat() == RECHERCHESATISFACTION);
+  CPPUNIT_ASSERT(ea2->getPositionCourante() == ea2->getPositionFinale());
+  CPPUNIT_ASSERT(p->obtenirCubePrioritaire() == ea5);
+
+//test d'une etape de resoudre
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == SATISFAIT && ea3->getEtat() == RECHERCHESATISFACTION && ea4->getEtat() == RECHERCHESATISFACTION && ea5->getEtat() == SATISFAIT && ea6->getEtat() == RECHERCHESATISFACTION);
+  CPPUNIT_ASSERT(ea5->getPositionCourante() == ea5->getPositionFinale());
+  CPPUNIT_ASSERT(p->obtenirCubePrioritaire() == ea6);
+
+//test d'une etape de resoudre
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == SATISFAIT && ea3->getEtat() == RECHERCHESATISFACTION && ea4->getEtat() == RECHERCHESATISFACTION && ea5->getEtat() == SATISFAIT && ea6->getEtat() == SATISFAIT);
+  CPPUNIT_ASSERT(ea6->getPositionCourante() == ea6->getPositionFinale());
+  CPPUNIT_ASSERT(p->obtenirCubePrioritaire() == ea3);
+
+//test d'une etape de resoudre
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == SATISFAIT && ea3->getEtat() == SATISFAIT && ea4->getEtat() == RECHERCHESATISFACTION && ea5->getEtat() == SATISFAIT && ea6->getEtat() == SATISFAIT);
+  CPPUNIT_ASSERT(ea3->getPositionCourante() == ea3->getPositionFinale());
+  CPPUNIT_ASSERT(p->obtenirCubePrioritaire() == ea4);
+
+//test d'une etape de resoudre
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == SATISFAIT && ea3->getEtat() == SATISFAIT && ea4->getEtat() == SATISFAIT && ea5->getEtat() == SATISFAIT && ea6->getEtat() == SATISFAIT);
+  CPPUNIT_ASSERT(ea4->getPositionCourante() == ea4->getPositionFinale());
+  CPPUNIT_ASSERT(p->obtenirCubePrioritaire() == NULL);
+
 }
 
+void plateformeMondeDesCubesTest::obtenirCubePrioritaireTest(void){
+  EcoAgent *temp = NULL;
+  // on remet a zero la plateforme
+  PlateformeMondeDesCubes::kill();
+  p = PlateformeMondeDesCubes::getInstance();
+  // on reprend l'exemple du rapport
+
+  ea1->setPositionCourante(*(p->getTableID()));
+  ea1->setPositionFinale(*(p->getTableID()));
+  
+  ea2->setPositionCourante(*(ea3->getId()));
+  ea2->setPositionFinale(*(ea1->getId()));
+
+  ea3->setPositionCourante(*(ea1->getId()));
+  ea3->setPositionFinale(*(ea2->getId()));
+
+  p->addEcoAgent(*ea1);
+  p->addEcoAgent(*ea2);
+  p->addEcoAgent(*ea3);
+  
+  // Test des Etat des EcoAgent avant initialisation
+  CPPUNIT_ASSERT(ea1->getEtat() == UNDEFINED && ea2->getEtat() == UNDEFINED && ea3->getEtat() == UNDEFINED);
+
+  p->initialiser();
+  // Test des Etat des EcoAgent apres initialisation
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == RECHERCHESATISFACTION && ea3->getEtat() == RECHERCHESATISFACTION);
+
+  //Test de la premiere etape de resolution
+  // Obtention du bon cube prioritaire
+  CPPUNIT_ASSERT( p->obtenirCubePrioritaire() == ea2);
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  // Changement d'Etat correct
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == RECHERCHESATISFACTION && ea3->getEtat() == RECHERCHEFUITE);
+  
+  // Obtention du bon cube prioritaire
+  CPPUNIT_ASSERT( p->obtenirCubePrioritaire() == ea3);
+
+  
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  //Changement d'Etat correct
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == RECHERCHEFUITE && ea3->getEtat() == RECHERCHEFUITE);
+  // Obtention du bon cube prioritaire
+  CPPUNIT_ASSERT(p->obtenirCubePrioritaire() == ea2);  
+
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  // Changement de position correct
+  CPPUNIT_ASSERT(ea2->getPositionCourante() == p->getTableID());
+  
+  // Changement d'Etat correct
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == RECHERCHESATISFACTION && ea3->getEtat() == RECHERCHEFUITE);
+  
+   // Obtention du bon cube prioritaire
+  CPPUNIT_ASSERT( p->obtenirCubePrioritaire() == ea3);
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+  
+  // Changement de position correct
+  CPPUNIT_ASSERT(ea3->getPositionCourante() == p->getTableID());
+
+  // Changement d'Etat correct
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == RECHERCHESATISFACTION && ea3->getEtat() == RECHERCHESATISFACTION);
+
+   // Obtention du bon cube prioritaire
+  CPPUNIT_ASSERT( p->obtenirCubePrioritaire() == ea2);
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+
+  // Changement de position correct
+  CPPUNIT_ASSERT(ea2->getPositionCourante() == ea2->getPositionFinale());
+
+  // Changement d'Etat correct
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == SATISFAIT && ea3->getEtat() == RECHERCHESATISFACTION);
+
+  // Obtention du bon cube prioritaire
+  CPPUNIT_ASSERT( p->obtenirCubePrioritaire() == ea3);
+  temp = p->obtenirCubePrioritaire();
+  temp->agir();
+
+  // Changement de position correct
+  CPPUNIT_ASSERT(ea3->getPositionCourante() == ea3->getPositionFinale());
+
+
+  // Changement d'Etat correct
+  CPPUNIT_ASSERT(ea1->getEtat() == SATISFAIT && ea2->getEtat() == SATISFAIT && ea3->getEtat() == SATISFAIT);
+
+  // Obtention du bon cube prioritaire
+  CPPUNIT_ASSERT( p->obtenirCubePrioritaire() == NULL);
+  
+}
