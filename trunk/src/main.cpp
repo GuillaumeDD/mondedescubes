0a0f31a399237961603cb0adb704fa06e410b069
guillaume.dd <guillaume.dd@f4401540-0d51-11de-9c5c-7db7966a26d7>
2009-05-04 20:09:26 +0000

    Fin de la lecture du fichier XML

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -5,12 +5,17 @@
 
 using namespace std;
 
-void init();
+void init(string);
 
 int main(int argc, char** argv){
   PlateformeMondeDesCubes *p = NULL;
+  string data = "data.xml";
   int temp;
-  init();
+
+  if(argc > 1){
+    data = argv[1];
+  }
+  init(data);
   p = PlateformeMondeDesCubes::getInstance();
   p->initialiser();
   cout << "INITIALISATION" << endl;
@@ -38,52 +43,11 @@ int main(int argc, char** argv){
     cout << "Regle non verifie !"<< endl;
   }
 
-  /*
-    EcoAgent *ea1 = NULL, *ea2 = NULL, *ea3 = NULL;
-
-    p = PlateformeMondeDesCubes::getInstance();
-    cout << p->toString()<< endl;
-
-    ea1 = new Cube();
-    ea2 = new Cube();
-    ea3 = new Cube();
-
-    p->addEcoAgent(*ea1);
-    p->addEcoAgent(*ea2);
-    p->addEcoAgent(*ea3);
-    ea1->setPositionCourante(*(p->getTableID()));
-    ea1->setPositionFinale(*(p->getTableID()));
-  
-    ea2->setPositionCourante(*(ea3->getId()));
-    ea2->setPositionFinale(*(ea1->getId()));
-
-    ea3->setPositionCourante(*(ea1->getId()));
-    ea3->setPositionFinale(*(ea2->getId()));
-
-    // lancement de la resolution
-    p->initialiser();
-    if(p->verifierCoherence()){
-    while(!p->sontSatisfaits()){
-    cout << p->toString()<<endl;
-    cout << "------------------------"<< endl;
-    cout << *p << endl;
-    cout << "------------------------"<< endl;
-    p->resoudre();
-    }
-    }
-  
-    cout << "SOLUTION FINALE :" << endl;
-    cout << p->toString()<<endl;
-    cout << "------------------------"<< endl;
-    cout << *p << endl;
-    cout << "------------------------"<< endl;
-    p->resoudre();
-  */
   return EXIT_SUCCESS;
 }
 
 
-void init(){
+void init(string file){
   PlateformeMondeDesCubes *p = NULL;
   EcoAgentID *eaid = NULL;
   Cube* c = NULL;
@@ -96,9 +60,10 @@ void init(){
 
 
   // Chargement du document XML
-  TiXmlDocument doc( "data.xml" );
+  TiXmlDocument doc( file );
   if(!doc.LoadFile()){
     cout << "Impossible de charger le fichier de donnees." << endl;
+    exit(0);
   }
 
   // Chargement de la table
@@ -121,6 +86,7 @@ void init(){
   elem = hdl.FirstChildElement("MondeDesCubes").FirstChildElement("CubeList").FirstChildElement().Element();
   if(!elem){
     cout << "Impossible de charger les cubes" << endl;
+    exit(0);
   }else{
 
     try{
@@ -158,6 +124,7 @@ void init(){
       }
     }catch(ExceptionEcoAgentDejaEnregistre ex){
       cout << "L'ajout d'un EcoAgent avec un ID attribue a un autre EcoAgent a ete tente. Chaque EcoAgent doit avoir un identifiant unique ! La resolution est donc impossible." << endl;
+      exit(0);
     }
   }
 
